c74f38b5424e7bc5b600c7ff2ff73995af3910b8
Barry Prendergast <barry.m.prendergast@gmail.com>
2023-11-11 12:30:09 +0100

    Completed code review recommedations.

--- game.js
+++ game.js
@@ -3,6 +3,13 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 const JUMP_VELOCITY = -330;
 const MOVE_VELOCITY = 160;
+const ANIM_KEYS = {
+    LEFT: 'left',
+    RIGHT: 'right',
+    TURN: 'turn',
+    DUCK: 'duck',
+    ATTACK: 'attack'
+};
 
 // Game configuration
 const config = {
@@ -19,7 +26,35 @@ const config = {
     scene: {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        handleInput: handleInput,
+
+        createSky: function () {
+            this.add.image(400, 300, 'sky');
+        },
+
+        createPlatforms: function () {
+            this.platforms = this.physics.add.staticGroup();
+            this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+            this.platforms.create(600, 400, 'ground');
+            this.platforms.create(50, 250, 'ground');
+            this.platforms.create(750, 220, 'ground');
+        },
+
+        createPlayer: function () {
+            this.player = this.physics.add.sprite(100, 450, 'dude');
+            this.player.setBounce(0.2);
+            this.player.setCollideWorldBounds(true);
+        },
+
+        setupCollisions: function () {
+            this.physics.add.collider(this.player, this.platforms);
+        },
+
+        setupControls: function () {
+            this.cursors = this.input.keyboard.createCursorKeys();
+            this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        }
     }
 };
 
@@ -35,79 +70,59 @@ function preload() {
         frameWidth: 32,
         frameHeight: 48
     });
+    // Consider adding error handling for asset loading
 }
 
-// Creating the game scene
 function create() {
-    // Add the sky
-    this.add.image(400, 300, 'sky');
-
-    // Add the platforms as a static group
-    const platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-    // Add the player
-    this.player = this.physics.add.sprite(100, 450, 'dude');
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-
-    // Add collisions between player and platforms
-    this.physics.add.collider(this.player, platforms);
-
-    // Set up the keyboard controls
-    this.cursors = this.input.keyboard.createCursorKeys();
-    this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-    // Create animations for the player
+    this.createSky();
+    this.createPlatforms();
+    this.createPlayer();
+    this.setupCollisions();
+    this.setupControls();
     createAnimations.call(this);
 }
 
-// Separate function to handle the creation of animations
 function createAnimations() {
     this.anims.create({
-        key: 'left',
+        key: ANIM_KEYS.LEFT,
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
-        key: 'turn',
+        key: ANIM_KEYS.TURN,
         frames: [{ key: 'dude', frame: 4 }],
         frameRate: 20
     });
     this.anims.create({
-        key: 'right',
+        key: ANIM_KEYS.RIGHT,
         frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
-        key: 'duck',
+        key: ANIM_KEYS.DUCK,
         frames: [{ key: 'dude', frame: 4 }],
         frameRate: 20
     });
     this.anims.create({
-        key: 'attack',
+        key: ANIM_KEYS.ATTACK,
         frames: this.anims.generateFrameNumbers('dude', { start: 9, end: 11 }),
         frameRate: 20,
         repeat: -1
     });
 }
 
-// Function to handle user input and control the player
 function handleInput() {
     if (this.cursors.left.isDown) {
         this.player.setVelocityX(-MOVE_VELOCITY);
-        this.player.anims.play('left', true);
+        this.player.anims.play(ANIM_KEYS.LEFT, true);
     } else if (this.cursors.right.isDown) {
         this.player.setVelocityX(MOVE_VELOCITY);
-        this.player.anims.play('right', true);
+        this.player.anims.play(ANIM_KEYS.RIGHT, true);
     } else {
         this.player.setVelocityX(0);
-        this.player.anims.play('turn');
+        this.player.anims.play(ANIM_KEYS.TURN);
     }
 
     if (this.cursors.up.isDown && this.player.body.touching.down) {
@@ -115,25 +130,24 @@ function handleInput() {
     }
 
     if (this.cursors.down.isDown) {
-        this.player.anims.play('duck', true);
+        this.player.anims.play(ANIM_KEYS.DUCK, true);
     } else {
-        if (this.player.anims.getCurrentKey() === 'duck') {
+        if (this.player.anims.getCurrentKey() === ANIM_KEYS.DUCK) {
             this.player.anims.stop();
             this.player.setTexture('dude', 4);
         }
     }
 
     if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
-        this.player.anims.play('attack', true);
+        this.player.anims.play(ANIM_KEYS.ATTACK, true);
     } else {
-        if (this.player.anims.getCurrentKey() === 'attack') {
+        if (this.player.anims.getCurrentKey() === ANIM_KEYS.ATTACK) {
             this.player.anims.stop();
             this.player.setTexture('dude', 4);
         }
     }
 }
 
-// Updating the game scene
 function update() {
-    handleInput.call(this);
+    this.handleInput();
 }
